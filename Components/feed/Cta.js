4befa850e8f7a781ac9a336db88ef67ffb26865d
Rownok <rownok2001@gmail.com>
2022-07-17 14:01:26 +0600

    rownok

--- Components/feed/Cta.js
+++ Components/feed/Cta.js
@@ -6,8 +6,8 @@ const Cta = () => {
     <div className="bg-white container mx-auto max-w-screen-xl -mb-7">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
         <div className="flex flex-col">
-          <p className="md:text-3xl font-bold">
-            Hello Good Morning Find Service <br /> What You Need
+          <p className="md:text-3xl text-[20px] font-bold">
+            Hello Good Morning Find Service <br className="hidden md:block" /> What You Need
           </p>
           <p className="font-normal text-sm text-[#666666] mt-5 hidden md:block">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
@@ -15,7 +15,7 @@ const Cta = () => {
           </p>
           <a
             href=""
-            className="btn btn-primary text-[12px] md:xl-lg capitalize font-normal rounded-[50px] mt-6 w-40"
+            className="btn btn-primary text-[12px] md:xl-lg capitalize font-normal rounded-[50px] mt-6 w-40 "
           >Create an Account</a>
         </div>
         <div className="w-full aspect-video lg:aspect-square relative overflow-hidden">
