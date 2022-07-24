66a653f5a9a652f69cf1be0aa61fa8236771ed77
Rownok Mahbub <rownok2001@gmail.com>
2022-07-24 15:48:29 +0600

    rownok

--- Components/multiple_dashboard/Account/AllTransaction.js
+++ Components/multiple_dashboard/Account/AllTransaction.js
@@ -161,14 +161,14 @@ const AllTransaction = ({goNext}) => {
               <img src="/Assets/icon/search.svg" width={18} height={28} alt='fdefd'/>
             </div>
           </div>
-          <div className="w-40 relative mt-2 mr-3">
+          <div className="w-40 relative mt-2 ">
             <input
               className="border border-primary focus:outline-primary rounded-md px-2 py-1.5"
               type="date"
               name="startDate"
               placeholder="Start Date"
             />
-            <span className="bg-primary text-white absolute -right-3 top-0 h-[40px] w-10 flex justify-center items-center text-xl rounded-r-md pointer-events-none">
+            <span className="bg-primary text-white absolute -right-0 top-0 h-[40px] w-10 flex justify-center items-center text-xl rounded-r-md pointer-events-none">
               <img className=" scale-90" src="/Assets/icon/calendar.svg" />
               
             </span>
@@ -178,7 +178,7 @@ const AllTransaction = ({goNext}) => {
         <div className="relative mt-1 z-20">
           <Listbox.Button className="relative w-full cursor-pointer rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm border border-primary">
             <span className="block truncate w-24">{selected.name}</span>
-            <div className="absolute top-0 -bottom-[1px] right-0 flex  items-center px-2 pointer-events-none bg-primary rounded-r-lg">
+            <div className="absolute top-0 -bottom-[1px] -right-[1px] flex  items-center px-2 pointer-events-none bg-primary rounded-r-lg">
                <img src="/Assets/icon/downarrow.svg" alt="" />
               </div>
           </Listbox.Button>
