e012c5da62e74be5974f9c374f7694f738919af3
Rownok <rownok2001@gmail.com>
2022-08-16 18:42:41 +0600

    rownok

--- Components/service/order/ServiceSelectType1.js
+++ Components/service/order/ServiceSelectType1.js
@@ -1,7 +1,7 @@
 import OptionItem from "./OptionItem";
 import { useState, useEffect } from "react";
 
-const ServiceSelectType1 = ({ data, onChange }) => {
+const ServiceSelectType1 = ({ data, onChange, notClickable }) => {
   const [selectedOptions, setSelectedOptions] = useState([]);
 
   const addToSelectedOptions = (id, title) => {
@@ -23,7 +23,7 @@ const ServiceSelectType1 = ({ data, onChange }) => {
   }, [selectedOptions]);
 
   return (
-    <>
+    <div className={`${notClickable && "pointer-events-none"}`}>
       <div className="text-center mb-8">
         <span className="border-b border-dashed capitalize px-8 pb-2">
           {data.category}
@@ -31,21 +31,12 @@ const ServiceSelectType1 = ({ data, onChange }) => {
       </div>
       <div className="flex flex-wrap gap-4">
         {data.options?.selectedOptions?.map((item, index) => (
-<<<<<<< HEAD
-          <span
-            className={`sm:px-8 text-primary px-4 py-2 border-2 text-sm sm:text-base rounded-full min-w-[70] sm:min-w-[150px]`}
-          >
-             {item.title}
-             {data.category}
-          </span>
-=======
           <OptionItem
             key={item.id}
             item={item}
             onClick={(data) => toggleSelectedOptions(data.id, data.title)}
             selected={selectedOptions.find((data) => data.id === item.id)}
           />
->>>>>>> 029ee1bae0171b89526af1201d5840ea47f12c48
         ))}
 
         {data.options?.customOptions?.map((item, index) => (
@@ -57,7 +48,7 @@ const ServiceSelectType1 = ({ data, onChange }) => {
           />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
